ae6b71d0066ecd0445d4fbbd5dd95c7032313048
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2023-10-18 15:23:15 +0600

    added banner section

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,28 @@
+
+const Banner = () => {
+      return (
+            <div className="bg-[#E7AB3C]"> {/* Replace "bg-primary" with the desired background color class */}
+                  <div className="max-w-7xl mx-auto">
+                        <div className="p-4 md:pt-8 md:pb-0">
+                              <div className="container mx-auto flex flex-col lg:flex-row items-center">
+                                    <div className="w-full md:w-4/5 lg:w-1/2 mb-4 lg:mb-0">
+                                          <img
+                                                src="https://i.ibb.co/qN3WbXH/header-img-png.webphttps://i.ibb.co/F3bP7Vt/header-img-png.webp"
+                                                alt="Banner Image"
+                                                className="w-full md:mr-4 rounded-lg"
+                                          />
+                                    </div>
+                                    <div className="w-full lg:mt-7 lg:w-1/2">
+                                          <span className="text-xl text-[#FFF] mr-2">Flat</span>
+                                          <span className="text-4xl text-white font-semibold">50%Off</span>
+                                          <h2 className="text-3xl md:text-5xl lg:text-6xl text-white my-5 font-bold mb-4">IT’S HAPPENING THIS SEASON!</h2>
+                                          <button className="cursor-pointer mt-5 bg-white text-black py-2 px-8 font-medium hover:border-2 hover:border-white hover:text-white hover:bg-[#E7AB3C] rounded-full text-xl">Purchase Now</button>
+                                    </div>
+                              </div>
+                        </div>
+                  </div>
+            </div>
+      );
+};
+
+export default Banner;
--- src/Shared/Menubar/Menubar.jsx
+++ src/Shared/Menubar/Menubar.jsx
@@ -14,11 +14,11 @@ const Menubar = () => {
             <div className="bg-[#4C4C4C]">
                   <div className="max-w-7xl mx-auto p-2">
                         <h3 onClick={handleMenu} className="cursor-pointer flex justify-end items-center text-[#FFFFFF] bg-black w-fit px-4 py-1 rounded-lg gap-2 text-xl text-end"><span><HiMenu></HiMenu></span>Menu</h3>
-                        <ul className={`${menu ? 'flex flex-col' : 'hidden'} text-[#FFF] font-medium`}>
-                              <NavLink className={({ isActive }) => isActive ? 'bg-[#CCCCCC]' : ''} to='/' ><li className="py-3 px-8 text-sm h-full ">HOME</li></NavLink>
-                              <NavLink className={({ isActive }) => isActive ? 'bg-[#CCCCCC]' : ''} to='/addProducts' ><li className="py-3 px-8 text-sm h-full">Add Products</li></NavLink>
-                              <NavLink className={({ isActive }) => isActive ? 'bg-[#CCCCCC]' : ''} to='/myCart' ><li className="py-3 px-8 text-sm h-full">My Cart</li></NavLink>
-                              <NavLink className={({ isActive }) => isActive ? 'bg-[#CCCCCC]' : ''} to='/login' ><li className="py-3 px-8 text-sm h-full">Login</li></NavLink>
+                        <ul className={`${menu ? 'flex flex-col' : 'hidden'} mt-3 text-[#FFF] font-medium`}>
+                              <NavLink className={({ isActive }) => isActive ? 'rounded-md bg-[#CCCCCC]' : ''} to='/' ><li className="py-3 px-8 text-sm h-full ">HOME</li></NavLink>
+                              <NavLink className={({ isActive }) => isActive ? 'rounded-md bg-[#CCCCCC]' : ''} to='/addProducts' ><li className="py-3 px-8 text-sm h-full">Add Products</li></NavLink>
+                              <NavLink className={({ isActive }) => isActive ? 'rounded-md bg-[#CCCCCC]' : ''} to='/myCart' ><li className="py-3 px-8 text-sm h-full">My Cart</li></NavLink>
+                              <NavLink className={({ isActive }) => isActive ? 'rounded-md bg-[#CCCCCC]' : ''} to='/login' ><li className="py-3 px-8 text-sm h-full">Login</li></NavLink>
                         </ul>
                   </div>
             </div>
