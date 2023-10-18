2f52ef24fa27a5162056d518536a1a683b6a2658
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2023-10-18 21:03:18 +0600

    added footer section

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 import Menubar from "../Shared/Menubar/Menubar";
+import Footer from "../Shared/Footer/Footer";
 
 const Root = () => {
       return (
@@ -9,6 +10,7 @@ const Root = () => {
                   <Navbar></Navbar>
                   <Menubar></Menubar>
                   <Outlet></Outlet>
+                  <Footer></Footer>
             </div>
       );
 };
