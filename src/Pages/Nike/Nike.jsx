71c9a91f0cd407e1de4a37169587710ba114602e
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2023-10-19 18:31:25 +0600

    added products based advertisement slider

--- src/Pages/Nike/Nike.jsx
+++ src/Pages/Nike/Nike.jsx
@@ -11,31 +11,25 @@ const Nike = () => {
                         <div id="slide1" className="carousel-item relative w-full">
                               <img src="https://i.ibb.co/MDFPjZq/nikeadd1.jpg" className="w-full h-[70vh]" />
                               <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                    <a href="#slide4" className="btn btn-circle">❮</a>
+                                    <a href="#slide3" className="btn btn-circle">❮</a>
                                     <a href="#slide2" className="btn btn-circle">❯</a>
                               </div>
                         </div>
-                        <div id="slide2" className="carousel-item relative w-full">
-                              <img src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+                        <div id="slide2" className="carousel-item relative w-full h-[70vh]">
+                              <img src="https://i.ibb.co/1ThRMx4/35463b103211209-5f8d57c277188.jpg" className="w-full" />
                               <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                                     <a href="#slide1" className="btn btn-circle">❮</a>
                                     <a href="#slide3" className="btn btn-circle">❯</a>
                               </div>
                         </div>
-                        <div id="slide3" className="carousel-item relative w-full">
-                              <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+                        <div id="slide3" className="carousel-item relative w-full h-[70vh]">
+                              <img src="https://i.ibb.co/5Kx3rsr/130214105230-pistorius-nike-ad.jpg" className="w-full" />
                               <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                                     <a href="#slide2" className="btn btn-circle">❮</a>
-                                    <a href="#slide4" className="btn btn-circle">❯</a>
-                              </div>
-                        </div>
-                        <div id="slide4" className="carousel-item relative w-full">
-                              <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
-                              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                    <a href="#slide3" className="btn btn-circle">❮</a>
                                     <a href="#slide1" className="btn btn-circle">❯</a>
                               </div>
                         </div>
+                       
                   </div>
                   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-10 max-w-7xl mx-auto">
                         {
